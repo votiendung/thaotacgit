11e3b8a80dd94b588e677b22cc51bfbcdcf884ed
gintk111 <thanhnguyentz1997@gmail.com>
2020-11-10 15:34:50 +0700

    dm

---
+++ src/main/java/GaiXinh.java
@@ -0,0 +1,85 @@
+public class GaiXinh {
+    String matxinh;
+    String vuto;
+    String chandai;
+    String ditto;
+    String thongminh;
+    String bayba1cachhoply;
+    String lamtinhgioi;
+    String rangkhenh;
+
+    public GaiXinh(String matxinh, String vuto, String chandai, String ditto, String thongminh, String bayba1cachhoply, String lamtinhgioi, String rangkhenh) {
+        this.matxinh = matxinh;
+        this.vuto = vuto;
+        this.chandai = chandai;
+        this.ditto = ditto;
+        this.thongminh = thongminh;
+        this.bayba1cachhoply = bayba1cachhoply;
+        this.lamtinhgioi = lamtinhgioi;
+        this.rangkhenh = rangkhenh;
+    }
+
+    public String getMatxinh() {
+        return matxinh;
+    }
+
+    public void setMatxinh(String matxinh) {
+        this.matxinh = matxinh;
+    }
+
+    public String getVuto() {
+        return vuto;
+    }
+
+    public void setVuto(String vuto) {
+        this.vuto = vuto;
+    }
+
+    public String getChandai() {
+        return chandai;
+    }
+
+    public void setChandai(String chandai) {
+        this.chandai = chandai;
+    }
+
+    public String getDitto() {
+        return ditto;
+    }
+
+    public void setDitto(String ditto) {
+        this.ditto = ditto;
+    }
+
+    public String getThongminh() {
+        return thongminh;
+    }
+
+    public void setThongminh(String thongminh) {
+        this.thongminh = thongminh;
+    }
+
+    public String getBayba1cachhoply() {
+        return bayba1cachhoply;
+    }
+
+    public void setBayba1cachhoply(String bayba1cachhoply) {
+        this.bayba1cachhoply = bayba1cachhoply;
+    }
+
+    public String getLamtinhgioi() {
+        return lamtinhgioi;
+    }
+
+    public void setLamtinhgioi(String lamtinhgioi) {
+        this.lamtinhgioi = lamtinhgioi;
+    }
+
+    public String getRangkhenh() {
+        return rangkhenh;
+    }
+
+    public void setRangkhenh(String rangkhenh) {
+        this.rangkhenh = rangkhenh;
+    }
+}
