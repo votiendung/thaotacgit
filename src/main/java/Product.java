d17d4e76cee46afcc7f7661765cb1de1ba79492e
votiendung <votiendung.92@gmail.com>
2020-11-10 15:57:47 +0700

    add pro

---
+++ src/main/java/Product.java
@@ -0,0 +1,11 @@
+public class Product {
+    private int ProID;
+    private String Name;
+    private double price;
+
+    public Product(int proID, String name, double price) {
+        ProID = proID;
+        Name = name;
+        this.price = price;
+    }
+}
